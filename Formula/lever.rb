4d7463d827c4ad660d721c274249972fa63b6060
Jordan Humphreys <mrsweaters@users.noreply.github.com>
2026-06-09 14:26:48 -0400

    lever 0.1.0

--- Formula/lever.rb
+++ Formula/lever.rb
@@ -14,22 +14,22 @@ class Lever < Formula
   on_macos do
     on_arm do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_MACOS_SHA256"
+      sha256 "a1430fafd46e8d3613edb09ae5a10b230505e82455bb8b360d2e5aa8bcea8a0c"
     end
     on_intel do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_MACOS_SHA256"
+      sha256 "1e364d6ff5b8d684a1813a5f4fb3cb75a44a587711fb3e5d8e3e55e3e16063c0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_LINUX_SHA256"
+      sha256 "3f2e9b59b3ff23046f069df9f1922f13437716871948bdfa8c7a6e6a269b717b"
     end
     on_intel do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_LINUX_SHA256"
+      sha256 "f877241d82e283082f14f9bfb9e9941646d164ea034441ce1087dab1a02cf8b4"
     end
   end
 
