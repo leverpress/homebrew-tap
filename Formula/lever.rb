b02a730db2017213e95d14ae63304b8d58098acb
Jordan Humphreys <mrsweaters@users.noreply.github.com>
2026-06-09 15:50:43 -0400

    lever 0.3.1

--- Formula/lever.rb
+++ Formula/lever.rb
@@ -8,28 +8,28 @@
 class Lever < Formula
   desc "Publish from files, not platforms — the lever.press CLI"
   homepage "https://lever.press"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-aarch64-apple-darwin.tar.gz"
-      sha256 "9440837c28f8a41fe116a44497c0aa5b04e806e9367de7cb7f07d14bc62dd195"
+      sha256 "3a8f55a133bf63c2dcb13c348a6f1150612a6be01b2d4babe68b43f415d999c7"
     end
     on_intel do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-x86_64-apple-darwin.tar.gz"
-      sha256 "6c11674775c296b5e7b6aa23a5f2686dc71a7c52b324488c1f1872efab642c99"
+      sha256 "efbdbf0842e59990a48a03411e8a1fa1b94f2151027403fac5b3caf470350b95"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0ce1ca5b61a036a446f536f3a37b21db0fd8811fc61abf944bf275fbffe6b406"
+      sha256 "d76040ca436f4b7968ae1848c68f8f6ddf564df3cb3a7168a52443e3e597d5fa"
     end
     on_intel do
       url "https://github.com/leverpress/lever-cli/releases/download/v#{version}/lever-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9b8d6ba0b9fdf50453931ea19a412ee5fe9cc85a04274810e9247fcdc8521644"
+      sha256 "c9dc7374b9e1324a128d0480cc134a6783ee4c62e2a74fa63370ee37548db58b"
     end
   end
 
